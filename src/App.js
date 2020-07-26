b7aa0d37ea46c8d51205d46c4709693b93504380
Andrew Smith <ajs3993@gmail.com>
2020-07-26 15:34:50 -0500

    card styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {MDBContainer,MDBRow,MDBCol,MDBCard,MDBView,MDBCardTitle,MDBCardBody,MDBCardGroup,MDBNavbar,MDBIcon,
-  MDBMask,MDBBtn,MDBNavbarBrand,MDBNavbarNav} from 'mdbreact';
+import {MDBContainer,MDBRow,MDBCol,MDBCard,MDBView,MDBCardTitle,MDBCardBody,MDBCardGroup,MDBIcon,
+  MDBMask,MDBBtn,MDBCardImage} from 'mdbreact';
   import './App.css';
 
 class App extends React.Component {
@@ -36,9 +36,11 @@ class App extends React.Component {
                     Former health professional who fell in to the world of software
                   </p>
                   <hr color='white'/>
+                  
                   <MDBBtn floating tag='a' color='blue'>
                     <MDBIcon fab icon='github' className='white-text' />
                   </MDBBtn>
+                  
                   <MDBBtn floating tag='a' color='blue'>
                     <MDBIcon fab icon='linkedin' className='white-text' />
                   </MDBBtn>
@@ -87,7 +89,10 @@ class App extends React.Component {
               </MDBCard>
 
             </MDBCol>
-            <MDBCol lg='8' md='12' className='text-center'>
+            
+            
+            
+            <MDBCol lg='8' md='12' className='text-center' >
               <div className='text-center mt-3 mb-5'>
                 <h1 className='white-text'>
                   
@@ -95,50 +100,61 @@ class App extends React.Component {
                   <hr color='white'/>
                 </h1>
               </div>
+              
               <MDBCardGroup deck>
                 
                 {/* card 1 */}
 
-                <MDBCard className='mb-5 rgba-white-light' narrow>
+                <MDBCard className=' mb-5 mdb-color darken-3' cascade>
                   <MDBView cascade hover>
-                    <img
+                    <MDBCardImage
                       src='https://i.imgur.com/Ya5rZqw.png?2'
-                      className='img-fluid'
+                      className='card-img-top'
                       alt='project one'
                     />
                     <a href='#!'>
                       <MDBMask overlay='white-slight' />
                     </a>
                   </MDBView>
-                  <MDBCardBody>
+                  <MDBCardBody className='pb-2'>
                     <h2 className='card-title white-text'>Pizza Time</h2>
-                    <hr className='my-3' />
+                    <hr color='white'/>
                     <p className='card-text white-text'>
-                    Pizza Time is a tool for restaurant owners to create their menu and sell their food. Pizza Time is also for users to order food directly from the app. It was developed with Mongoose, Express, React, and Node. It is currently still a work in progress.
+                    Pizza Time is a tool for restaurant owners to create their menu and sell their food. Pizza Time is also for users to order food directly from the app. It was developed with Mongoose, Express and React.
                     </p>
-                  </MDBCardBody>
-                  <hr className='m-3' />
-                  <MDBCardBody className='my-n4 rgba-white-dark'>
-                    <span className='float-left'>
+                  
+                  <hr color='white'/>
+           
+<h5 className='d-flex justify-content-between'>
+
+                    <div>
                       
-                        <MDBIcon fab size='2x' icon='js indigo-text' className='mr-2' />
+                       
+                        <MDBIcon fab icon='js yellow-text' className='mr-2' />
                      
                 
-                        <MDBIcon fab size='2x' icon='react cyan-text' className='mr-0' />
-            
-                      <img className='img-fluid d-inline' width= "100px" src='https://i.imgur.com/PCtjYTY.png'/>
-                    </span>
-                    <span className='float-right'>
-                      <a href='#!' className='waves-effect p-2 white-text animated pulse infinite'>
-                        Live Preview <MDBIcon icon='image white-text' className='ml-1' />
+                        <MDBIcon fab icon='react cyan-text' />
+                      
+                    </div>
+                    <div>
+                    
+                      <a href='#!' className='waves-effect white-text'>
+                     
+                        Live Preview <MDBIcon icon='image white-text' />
+                  
                       </a>
-                    </span>
+                    </div>
+
+
+                    </h5>
                   </MDBCardBody>
                 </MDBCard>
 
+                
+
               {/* card 2 */}
                 
-                <MDBCard className='mb-5 rgba-white-light' narrow>
+                <MDBCard className='mb-5 mdb-color darken-3' cascade>
                   <MDBView cascade hover>
                     <img
                       src='https://i.imgur.com/QwLb70q.png?4'
@@ -149,26 +165,35 @@ class App extends React.Component {
                       <MDBMask overlay='white-slight' />
                     </a>
                   </MDBView>
-                  <MDBCardBody>
+                  <MDBCardBody className='pb-2'>
                     <h2 className='card-title white-text'>Groupie</h2>
-                    <hr className='my-3' />
+                    <hr color='white'/>
                     <p className='card-text white-text'>
-                    With this app, keeping tabs on your favorite local artists just got a lot easier. Simply create an account and search events or artists in your area. Add your favorite artists and keep track of their upcoming events. Add their events to your schedule. Never miss out on you favorite events again.
+                    keeping tabs on your favorite local artists just got a lot easier. Simply create an account and search events or artists in your area. Add your favorite artists and keep track of their upcoming events.
                     </p>
-                  </MDBCardBody>
-                  <hr className='m-3' />
-                  <MDBCardBody className='my-n4 rgba-white-dark'>
-                    <span className='float-left pt-2'>
-        
-                        <MDBIcon fab size='2x' icon='python amber-text' className='mr-2 mb-3' />
-              
-                    </span>
-                    <span className='float-right'>
-                      <a href='#!' className='waves-effect p-2 white-text animated pulse infinite'>
-                        Live Preview <MDBIcon icon='image white-text' className='ml-1' />
-                      </a>
-                    </span>
-                  </MDBCardBody>
+                    <hr color='white'/>
+                    <h5 className='d-flex justify-content-between'>
+
+<div>
+  
+   
+    <MDBIcon fab  icon='python yellow-text md' />
+ 
+
+  
+</div>
+<div>
+
+  <a href='#!' className='waves-effect white-text'>
+  
+    Live Preview <MDBIcon icon='image white-text' />
+
+  </a>
+</div>
+
+
+</h5>
+</MDBCardBody>
                 </MDBCard>
               
               </MDBCardGroup>
@@ -179,7 +204,7 @@ class App extends React.Component {
 
               {/* card 3 */}
 
-                <MDBCard className='mb-5 rgba-white-light' narrow>
+                <MDBCard className='mb-5 mdb-color darken-3' cascade>
                   <MDBView cascade hover>
                     <img
                       src='https://i.imgur.com/YiT80Ww.png?1'
@@ -190,33 +215,39 @@ class App extends React.Component {
                       <MDBMask overlay='white-slight' />
                     </a>
                   </MDBView>
-                  <MDBCardBody>
+                  <MDBCardBody className='pb-2'>
                     <h2 className='card-title white-text'>Mongoose Flights</h2>
-                    <hr className='my-3' />
+                    <hr color='white'/>
                     <p className='card-text white-text'>
-                      A flight tracking app that uses MongoDB to hold data. I built it to practice with MongoDB so it has a bit more functionality than a real similar app would have.
-                    </p>
-                  </MDBCardBody>
-                  <hr className='m-3' />
-                  <MDBCardBody className='my-n4 rgba-white-dark'>
-                    <span className='float-left'>
+                      A flight tracking app that uses MongoDB to hold data. I built it to practice so it has a bit more functionality than a real app would have.</p>
+                    <hr color='white'/>
+                    
+                    <h5 className='d-flex justify-content-between'>
+
+                    <div>
                       
-                        <MDBIcon fab size='2x' icon='js indigo-text' className='mr-2' />
+                       
+                        <MDBIcon fab icon='js yellow-text' className='mr-2' />
+                     
                       
-                      
-                      <img className='img-fluid d-inline' width= "100px" src='https://i.imgur.com/PCtjYTY.png'/>
-                    </span>
-                    <span className='float-right'>
-                      <a href='#!' className='waves-effect p-2 white-text animated pulse infinite'>
-                        Live Preview <MDBIcon icon='image white-text' className='ml-1' />
+                    </div>
+                    <div>
+                    
+                      <a href='#!' className='waves-effect white-text'>
+                     
+                        Live Preview <MDBIcon icon='image white-text' />
+                  
                       </a>
-                    </span>
+                    </div>
+
+
+                    </h5>
                   </MDBCardBody>
                 </MDBCard>
 
                 {/* card 4 */}
 
-                <MDBCard className='mb-5 rgba-white-light' narrow>
+                <MDBCard className='mb-5 mdb-color darken-3' cascade>
                   <MDBView cascade hover>
                     <img
                       src='https://i.imgur.com/r414M0G.png?2'
@@ -227,28 +258,35 @@ class App extends React.Component {
                       <MDBMask overlay='white-slight' />
                     </a>
                   </MDBView>
-                  <MDBCardBody>
+                  <MDBCardBody className='pb-2'>
                     <h2 className='card-title white-text'>My Book Shelf</h2>
-                    <hr className='my-3' />
+                    <hr color='white'/>
                     <p className='card-text white-text'>
-                    MyBookShelf is your own personal library tracker. Sort your books to help you remember which books you have read and which books you haven't.
-                    </p>
-                  </MDBCardBody>
-                  <hr className='m-3' />
-                  <MDBCardBody className='my-n4 rgba-white-dark'>
-                    <span className='float-left'>
+                    MyBookShelf is your own personal library tracker. Sort your books to help you remember which books you have read and which books you havent. </p>
+                    <hr color='white'/>
                     
-                        <MDBIcon fab size='2x' icon='js indigo-text' className='mr-2' />
-              
-                      <img className='img-fluid d-inline' width= "100px" src='https://i.imgur.com/PCtjYTY.png'/>
-                    </span>
-                    <span className='float-right'>
-                      <a href='#!' className='waves-effect p-2 white-text animated pulse infinite'>
-                        Live Preview <MDBIcon icon='image white-text' className='ml-1' />
-                      </a>
-                    </span>
-                  </MDBCardBody>
-                </MDBCard>
+                    <h5 className='d-flex justify-content-between'>
+
+<div>
+  
+   
+    <MDBIcon fab icon='js yellow-text' className='mr-2' />
+ 
+  
+</div>
+<div>
+
+  <a href='#!' className='waves-effect white-text'>
+ 
+    Live Preview <MDBIcon icon='image white-text' />
+
+  </a>
+</div>
+
+
+</h5>
+</MDBCardBody>
+</MDBCard>
 
 
 
