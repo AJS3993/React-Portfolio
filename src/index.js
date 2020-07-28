24d9488fd9875344b9969531271bcb2df3bee10c
Andrew Smith <ajs3993@gmail.com>
2020-07-28 08:06:04 -0500

    merge

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import './index.css';
+import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
@@ -15,3 +16,8 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root')
   );
+
+  // If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
